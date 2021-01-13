0fa3ae589c05e3a0b9a6466289f1dba302b8f88a
Jason Du <jdu@mydevices.com>
2021-01-12 17:45:06 -0800

    fix: change field name

--- pkg/proto/state/v1/types.pb.validate.go
+++ pkg/proto/state/v1/types.pb.validate.go
@@ -157,13 +157,13 @@ func (m *DeviceStates) Validate() error {
 		return nil
 	}
 
-	for idx, item := range m.GetData() {
+	for idx, item := range m.GetList() {
 		_, _ = idx, item
 
 		if v, ok := interface{}(item).(interface{ Validate() error }); ok {
 			if err := v.Validate(); err != nil {
 				return DeviceStatesValidationError{
-					field:  fmt.Sprintf("Data[%v]", idx),
+					field:  fmt.Sprintf("List[%v]", idx),
 					reason: "embedded message failed validation",
 					cause:  err,
 				}
